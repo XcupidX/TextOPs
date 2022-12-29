ea09bc3ef9abf1b428434762a845ca53c8651773
XcupidX <adityaramdeosingh@gmail.com>
2022-12-29 12:08:32 +0530

    For AWS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,9 @@ export default function Navbar(props) {
                             <Link className="nav-link" to="/about">{props.aboutText}</Link>
                         </li>
                     </ul>
-                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                    <div className={`form-check text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="radio" id="flexSwitchCheckChecked" checked/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckChecked" >Enable DarkMode</label>
                     </div>
                 </div>
             </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,6 +30,22 @@ export default function TextForm(props) {
         props.showAlert("Copied to Clipboard!", "success");
     }
 
+    const speakText = () => {
+        let msg = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(msg);
+        const toogle = document.getElementById('toggle')
+        if (toogle.textContent === "Speak") {
+            toogle.innerHTML = "Stop"
+        }
+        else {
+            toogle.innerHTML = "Speak"
+            if (toogle.innerHTML === "Speak") {
+                window.speechSynthesis.cancel()
+            }
+        }
+        
+    }
+
     // Credits: Coding Wala
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
@@ -45,11 +61,12 @@ export default function TextForm(props) {
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
             <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3"> 
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#2d2f30':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" id="toggle" type="submit" onClick={speakText}>Speak</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
